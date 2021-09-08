4de6e20e1e5db1b457d1f103a3fefc89effe69f3
augusticor <rojascruzoscar@gmail.com>
2021-09-07 20:43:45 -0500

    Started <Linktree /> component
    - Created a header with img and @augusticor inside
    - img with border radius
    - username with temporal font

---
+++ src/LinkTree.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './link-tree-styles.css';
+
+const LinkTree = () => {
+	return (
+		<>
+			<header className='info'>
+				<img className='image' src='https://d1fdloi71mui9q.cloudfront.net/7TrNrWjiTk6X2Y3iJnXE_QlLnDOA3CHLK0c8j' alt='User profile photography' />
+				<h1 className='username'>@augusticor</h1>
+			</header>
+		</>
+	);
+};
+
+export default LinkTree;
