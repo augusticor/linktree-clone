13a31ff80a4221417608a4f0087c1bde9c81a6b3
augusticor <rojascruzoscar@gmail.com>
2021-09-09 21:35:10 -0500

    Adding data for links and rendering links

--- src/components/main/LinksGrid.js
+++ src/components/main/LinksGrid.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import LinkItem from './LinkItem';
 
+import { linksInfo } from '../../data/links-data';
+
 const LinksGrid = () => {
 	return (
 		<main>
-			<LinkItem
-				link={'https://www.linkedin.com/in/augusticor/'}
-				icon={'https://assets.production.linktr.ee/ef928851e29399340d4af70da9c77a3f60e5afad/icons/tabler-icons/brand-linkedin.svg'}
-				linklabel={'LinkedIn'}
-			/>
+			{linksInfo.map(({ id, link, icon, label }) => (
+				<LinkItem key={id} link={link} icon={icon} linklabel={label} />
+			))}
 		</main>
 	);
 };
---
+++ src/data/links-data.js
@@ -0,0 +1,40 @@
+const assetsBaseURL = 'https://assets.production.linktr.ee/ef928851e29399340d4af70da9c77a3f60e5afad/icons/tabler-icons/brand-';
+
+export const linksInfo = [
+	{
+		id: 'link-linkedin',
+		link: 'https://www.linkedin.com/in/augusticor/',
+		icon: `${assetsBaseURL}linkedin.svg`,
+		label: 'LinkedIn',
+	},
+	{
+		id: 'link-github',
+		link: 'https://github.com/augusticor',
+		icon: `${assetsBaseURL}github.svg`,
+		label: 'GitHub',
+	},
+	{
+		id: 'link-youtube',
+		link: 'https://www.youtube.com/channel/UC0_5DKWPKfTCu6S8Bs1QihA',
+		icon: `${assetsBaseURL}youtube.svg`,
+		label: 'YouTube',
+	},
+	{
+		id: 'link-twitter',
+		link: 'https://twitter.com/augusticor',
+		icon: `${assetsBaseURL}twitter.svg`,
+		label: 'Twitter',
+	},
+	{
+		id: 'link-duolingo',
+		link: 'https://www.duolingo.com/profile/augusticor',
+		icon: `${assetsBaseURL}language.svg`,
+		label: 'Duolingo',
+	},
+	{
+		id: 'link-linktree',
+		link: 'https://linktr.ee/augusticor',
+		icon: `${assetsBaseURL}linktree.svg`,
+		label: 'Real linktr.ee',
+	},
+];
