4372816beac8a9888b6bec8a149acbf7e1719209
augusticor <rojascruzoscar@gmail.com>
2023-02-23 11:33:27 -0500

    Change base property on vite.config.js to find assets on ghpages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   test: {
     environment: 'happy-dom',
     globals: true,
